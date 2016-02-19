379b99294ce92e656402ec566754e2f6ec132f22
David Remington <david.ian.remington@gmail.com>
2016-02-19 09:27:34 -0800

    add Word object

---
+++ src/main/java/Word.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class Word {
+
+  private static ArrayList<Word> instances = new ArrayList<Word>();
+
+  private String mWord;
+  private int mId;
+  private ArrayList<Definition> mDefinitions;
+
+  public Word(word) {
+    mWord = word;
+    instances.add(this);
+    mId = instances.size();
+    mDefinitions = new ArrayList<Definition>();
+  }
+
+  public String getWord() {
+    return mWord;
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public ArrayList<Definition> getDefinitions() {
+    return mDefinitions;
+  }
+
+  public void addDefinition(Definition definition) {
+    mDefinitions.add(definition);
+  }
+
+  public static ArrayList<Word> all() {
+    return instances;
+  }
+
+  public static Word find(int id) {
+    try {
+      return instances.get(id - 1);
+    } catch (IndexOutOfBoundsException e){
+      return null;
+    }
+  }
+
+  public static void clear {
+    instances.clear();
+  }
+  
+}
