6ae35bddecc8fc335dc4e98c8f2325c22d6c5bba
David Remington <david.ian.remington@gmail.com>
2016-02-19 11:56:44 -0800

    add working spark logic for words

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,5 +5,47 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/words.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+  //WORD LOGIC//
+
+    get("/words", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("words", Word.all());
+      model.put("template", "templates/words.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/words", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String word = request.queryParams("word");
+      Word newWord = new Word(word);
+      model.put("words", Word.all());
+      model.put("word", newWord);
+      model.put("template", "templates/words.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/words/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Word word = Word.find(Integer.parseInt(request.params(":id")));
+      ArrayList<Definition> definitions = word.getDefinitions();
+      model.put("word", word);
+      model.put("definitions", definitions);
+      model.put("template", "templates/tasks.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+  //DEFINITIONS LOGIC//
+
+    
+  }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,6 +1,7 @@
 import org.fluentlenium.adapter.FluentTest;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.Rule;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static org.fluentlenium.core.filter.FilterConstructor.*;
@@ -15,6 +16,15 @@ public class AppTest extends FluentTest {
     return webDriver;
   }
 
+  @Rule
+  public ClearRule clearRule = new ClearRule();
+
   @ClassRule
   public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Dictionary Creator");
+  }
 }
